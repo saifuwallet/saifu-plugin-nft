e8aa41f70457582350b0e7e5a9cf6772e98d3b91
David Mohl <git@d.sh>
2022-04-24 19:41:26 +0900

    Add link to token detail

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import { useState } from 'react';
-import { useTokenMetadata } from 'saifu';
+import { Link, useTokenMetadata } from 'saifu';
 
 export interface NftBoxProps {
   mint: string;
@@ -15,28 +15,32 @@ const NFTCard = ({ mint }: NftBoxProps) => {
   }
 
   return (
-    <div
-      className={clsx(
-        'shadow-md rounded-xl overflow-hidden break-inside-avoid-column',
-        metadata.isLoading && 'animate-pulse'
-      )}
-    >
-      <img
-        src={metadata.data?.image}
+    <Link to={`/tokens/${mint}`}>
+      <div
         className={clsx(
-          'object-cover aspect-auto w-full',
-          (!imgLoaded || metadata.isLoading) && 'bg-gray-500 h-52'
+          'shadow-md rounded-xl overflow-hidden break-inside-avoid-column',
+          metadata.isLoading && 'animate-pulse'
         )}
-        loading="lazy"
-        alt={metadata.data?.name}
-        onLoad={() => setImgLoaded(true)}
-      />
-      <div className="p-2 rounded-md">
-        <p className={clsx('font-bold', metadata.isLoading && 'h-4 w-full bg-gray-400 rounded-sm')}>
-          {metadata.data?.name}
-        </p>
+      >
+        <img
+          src={metadata.data?.image}
+          className={clsx(
+            'object-cover aspect-auto w-full',
+            (!imgLoaded || metadata.isLoading) && 'bg-gray-500 h-52'
+          )}
+          loading="lazy"
+          alt={metadata.data?.name}
+          onLoad={() => setImgLoaded(true)}
+        />
+        <div className="p-2 rounded-md">
+          <p
+            className={clsx('font-bold', metadata.isLoading && 'h-4 w-full bg-gray-400 rounded-sm')}
+          >
+            {metadata.data?.name}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
